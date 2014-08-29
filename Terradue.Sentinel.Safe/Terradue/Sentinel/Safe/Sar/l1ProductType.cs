599fba76e6be173b318ee2550cf63f0f07950b81
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-08-29 15:32:44 +0200

    Renamed to Safe
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

--- Terradue.Sentinel.Safe/Terradue/Sentinel/Safe/Sar/l1ProductType.cs
+++ Terradue.Sentinel.Safe/Terradue/Sentinel/Safe/Sar/l1ProductType.cs
@@ -4,16 +4,16 @@ using System.Xml;
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
 
-namespace Terradue.ESA.Sentinel.Classes.S1 {
+namespace Terradue.Sentinel.Safe.Sar {
     public partial class l1ProductType {
 
         public static l1ProductType FromAnnotationXml(string filename){
 
             FileStream annotationXmlStream = File.OpenRead(filename);
             XmlReader annotationXmlReader = XmlReader.Create(annotationXmlStream);
-            XmlSerializer serializer = new XmlSerializer(typeof(Terradue.ESA.Sentinel.Classes.S1.l1ProductType));
+            XmlSerializer serializer = new XmlSerializer(typeof(l1ProductType));
 
-            Terradue.ESA.Sentinel.Classes.S1.l1ProductType product = (Terradue.ESA.Sentinel.Classes.S1.l1ProductType)serializer.Deserialize(annotationXmlReader);
+            l1ProductType product = (l1ProductType)serializer.Deserialize(annotationXmlReader);
 
             return product;
 
--- Terradue.Sentinel.Safe/Terradue/Sentinel/Safe/Tests/ProductTests.cs
+++ Terradue.Sentinel.Safe/Terradue/Sentinel/Safe/Tests/ProductTests.cs
@@ -1,8 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
-using Terradue.ESA.Sentinel.Classes.S1;
+using Terradue.Sentinel.Safe.Sar;
 
-namespace Terradue.ESA.Sentinel {
+namespace Terradue.Sentinel.Safe.Tests {
 
     [TestFixture()]
     public class ProductTests {
@@ -10,7 +10,7 @@ namespace Terradue.ESA.Sentinel {
         [Test()]
         public void DeserializeL1ProductType() {
 
-            l1ProductType product = l1ProductType.FromAnnotationXml("../Terradue/ESA/Sentinel/Samples/rs2-iw1-slc-hh-20130426t045831-20130426t045914-001771-000001-001.xml");
+            l1ProductType product = l1ProductType.FromAnnotationXml("../Terradue/Sentinel/Safe/Sar/Samples/rs2-iw1-slc-hh-20130426t045831-20130426t045914-001771-000001-001.xml");
             Assert.AreEqual(product.adsHeader.startTime, DateTime.Parse("2013-04-26T04:58:31.024003"), "Error reading the start date of the product");
 
 
