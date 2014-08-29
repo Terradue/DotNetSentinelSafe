e2417b56922d68295466cc9ad68c57b96c29e417
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-08-29 15:17:36 +0200

    Initial commit
    Able to read Sentinel1 Level1 Products
    
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

---
+++ Terradue.ESA.Sentinel/Terradue/ESA/Sentinel/Tests/ProductTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using Terradue.ESA.Sentinel.Classes.S1;
+
+namespace Terradue.ESA.Sentinel {
+
+    [TestFixture()]
+    public class ProductTests {
+
+        [Test()]
+        public void DeserializeL1ProductType() {
+
+            l1ProductType product = l1ProductType.FromAnnotationXml("../Terradue/ESA/Sentinel/Samples/rs2-iw1-slc-hh-20130426t045831-20130426t045914-001771-000001-001.xml");
+            Assert.AreEqual(product.adsHeader.startTime, DateTime.Parse("2013-04-26T04:58:31.024003"), "Error reading the start date of the product");
+
+
+        }
+    }
+}
+
---
+++ Terradue.ESA.Sentinel/Terradue/ESA/Sentinel/l1ProductType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace Terradue.ESA.Sentinel.Classes.S1 {
+    public partial class l1ProductType {
+
+        public static l1ProductType FromAnnotationXml(string filename){
+
+            FileStream annotationXmlStream = File.OpenRead(filename);
+            XmlReader annotationXmlReader = XmlReader.Create(annotationXmlStream);
+            XmlSerializer serializer = new XmlSerializer(typeof(Terradue.ESA.Sentinel.Classes.S1.l1ProductType));
+
+            Terradue.ESA.Sentinel.Classes.S1.l1ProductType product = (Terradue.ESA.Sentinel.Classes.S1.l1ProductType)serializer.Deserialize(annotationXmlReader);
+
+            return product;
+
+        }
+
+    }
+}
+
